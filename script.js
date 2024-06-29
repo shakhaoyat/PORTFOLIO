fbd96c872b8a808649f257f85440819a64db0cb9
shakhaoyat <shakhaoyathossain.12@gmail.com>
2024-06-30 02:34:46 +0600

    1

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("hello chad ");
+function handleContact(event){
+    event.preventDefault();
+    console.log("Contact");
+    //console.log("event");
+    console.log(event.target);
+   // console.log(event.target.name);
+    //console.log(event.target.email);
+   // console.log(event.target.message);
+
+
+    console.log(event.target.name.value);
+    console.log(event.target.email.value);
+    console.log(event.target.message.value);
+
+}
